9e7817ed46421b3a041ff0e15e83e9fbeafe8540
Alan Johnson <alanjohnson@Alans-MacBook-Pro.local>
2022-07-08 08:51:00 -0400

    chapter on variables

---
+++ Intro_to_RB/variables/name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name? "
+first = gets.chomp
+puts "What is your last name? "
+last = gets.chomp
+puts "So, your full name is #{first} #{last}. \n Nice to meet you."
+10.times do
+  puts first
+end
+name1 = first
+name2 = last
+puts name1 + name2
